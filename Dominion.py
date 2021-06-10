f67ff7bce23324e2e69bb9bde3432701cd37531d
Derek <derekyun820@gmail.com>
2021-06-10 03:52:47 -0500

    initial commit

--- Dominion.py
+++ Dominion.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import tkinter
+
 
 
 root = Tk()
@@ -6,28 +8,66 @@ root.geometry("1500x1000")
 root.title("Dominion")
 
 
+counter = tkinter.IntVar()
+def onClick(event=None):
+    counter.set(counter.get() - 1)
 
-Bcurses = Button(root, text="Curses", padx=50, pady=50)
-Bestate = Button(root, text="Estate", padx=50, pady=50)
-Bduchy = Button(root, text="Duchy", padx=50, pady=50)
-Bprovince = Button(root, text="Province", padx=50, pady=50)
-Bcolony = Button(root, text="Colony", padx=50, pady=50)
-Bcurses.place(x=350, y=0)
+def onClick2(event=None):
+    counter.set(counter.get() + 1)
+
+def onClick3(event=None):
+    counter.set(counter.get() + 3)
+
+def onClick4(event=None):
+    counter.set(counter.get() + 6)
+
+def onClick5(event=None):
+    counter.set(counter.get() + 10)
+
+counter2 = tkinter.IntVar()
+def onClick6(event=None):
+    counter2.set(counter2.get() - 1)
+
+def onClick7(event=None):
+    counter2.set(counter2.get() + 1)
+
+def onClick8(event=None):
+    counter2.set(counter2.get() + 3)
+
+def onClick9(event=None):
+    counter2.set(counter2.get() + 6)
+
+def onClick10(event=None):
+    counter2.set(counter2.get() + 10)
+
+
+tkinter.Label(root, textvariable=counter).pack()
+tkinter.Label(root, textvariable=counter2).pack()
+
+
+# image = Image.open(file = r"C:\Users\Derek\Desktop\pythonProject\Province.png")
+# # photo = PhotoImage(file = r"C:\Users\Derek\Desktop\pythonProject\Province.png")
+# resize_image = image.resize((250, 250))
+Bcurses = Button(root, text="Curses", command=onClick)
+Bestate = Button(root, text="Estate", command=onClick2)
+Bduchy = Button(root, text="Duchy", command=onClick3)
+Bprovince = Button(root, text="Province", command=onClick4)
+Bcolony = Button(root, text="Colony", command=onClick5)
+Bcurses.place(x=0, y=0)
 Bestate.place(x=0, y=250)
 Bduchy.place(x=0, y=450)
 Bprovince.place(x=0, y=600)
 Bcolony.place(x=0, y=850)
 
-Bcurses2 = Button(root, text="Curses", padx=50, pady=50)
-Bestate2 = Button(root, text="Estate", padx=50, pady=50)
-Bduchy2 = Button(root, text="Duchy", padx=50, pady=50)
-Bprovince2 = Button(root, text="Province", padx=50, pady=50)
-Bcolony2 = Button(root, text="Colony", padx=50, pady=50)
-Bcurses2.place(x=350, y=350)
+Bcurses2 = Button(root, text="Curses", command=onClick6)
+Bestate2 = Button(root, text="Estate", command=onClick7)
+Bduchy2 = Button(root, text="Duchy", command=onClick8)
+Bprovince2 = Button(root, text="Province", command=onClick9)
+Bcolony2 = Button(root, text="Colony", command=onClick10)
+Bcurses2.place(x=1350, y=0)
 Bestate2.place(x=1350, y=250)
 Bduchy2.place(x=1350, y=450)
 Bprovince2.place(x=1350, y=600)
 Bcolony2.place(x=1350, y=850)
-
 
 root.mainloop()
