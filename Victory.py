886ec0337443ca0861460e44cb8f0352c0dff085
Daniel Yun <1danielyun@gmail.com>
2021-06-23 16:47:16 -0500

    oof

---
+++ Victory.py
@@ -0,0 +1,6 @@
+class Victory:
+  def __init__(self, name, value, cost, total_duchy):
+    self.name = name
+    self.value = value
+    self.cost = cost
+    self.total_duchy = total_duchy
--- main.py
+++ main.py
@@ -1,119 +1,77 @@
-
-
-
 from tkinter import *
 import tkinter
 from tkinter import Tk, Canvas, Frame, BOTH
+from Victory import Victory
 
 root = Tk()
 root.geometry("1920x1080")
 root.title("Dominion Counter")
-# bg = PhotoImage(file = r"C:\Users\Derek\Desktop\DominionVictory\background.png")
-# background_image = bg.zoom(2,2)
-# background_image.place(x = 0, y = 0)
-# test1 = Label(root, bg = r"C:\Users\Derek\Desktop\DominionVictory\background.png")
-# test1.place(x=0, y=0)
-es_temp = PhotoImage(file = r"C:\Users\Derek\Desktop\DominionVictory\background.png")
+es_temp = PhotoImage(file = r"Images\background.png")
 es = es_temp.zoom(2,2)
 coun = Label(root, image = es)
 coun.place(x=0, y = 0)
 
+# Totals
+totalduchy1 = 0
+counter = tkinter.IntVar()
+counter2 = tkinter.IntVar()
 
-estate_temp = PhotoImage(file = r"C:\Users\Derek\Desktop\DominionVictory\estate.png")
+# Images
+estate_temp = PhotoImage(file = r"Images\estate.png")
 estate = estate_temp.subsample(3)
-curse_temp = PhotoImage(file = r"C:\Users\Derek\Desktop\DominionVictory\curse.png")
+curse_temp = PhotoImage(file = r"Images\curse.png")
 curse = curse_temp.subsample(3)
-duchy_temp = PhotoImage(file = r"C:\Users\Derek\Desktop\DominionVictory\duchy.png")
+duchy_temp = PhotoImage(file = r"Images\duchy.png")
 duchy = duchy_temp.subsample(3)
-duke_temp = PhotoImage(file = r"C:\Users\Derek\Desktop\DominionVictory\duke.png")
+duke_temp = PhotoImage(file = r"Images\duke.png")
 duke = duke_temp.subsample(3)
-province_temp = PhotoImage(file = r"C:\Users\Derek\Desktop\DominionVictory\province.png")
+province_temp = PhotoImage(file = r"Images\province.png")
 province = province_temp.subsample(3)
-province_temp = PhotoImage(file = r"C:\Users\Derek\Desktop\DominionVictory\colony.png")
+province_temp = PhotoImage(file = r"Images\colony.png")
 colony = province_temp.subsample(3)
 
-totalduchy1 = 0
-
-class Victory:
-  def __init__(self, name, value, cost, total_duchy):
-    self.name = name
-    self.value = value
-    self.cost = cost
-    self.total_duchy = total_duchy
-  def total():
-  	global totalduchy1
-  	totalduchy1 = totalduchy1 + 1
+# Cards
+curse_card = Victory("Curse", -1, 0, 0)
+estate_card = Victory("Estate", 1, 2, 0)
+duchy_card = Victory("Duchy", 3, 5, totalduchy1)
+province_card = Victory("Province", 6, 8, 0)
+colony_card = Victory("Colony", 10, 11, 0)
 
 
-p1 = Victory("Estate", 1, 2, 0)
-p2 = Victory("Duchy", 3, 5, totalduchy1)
-p1 = Victory("Province", 6, 8, 0)
-p1 = Victory("Colony", 10, 11, 0)
-
-print(p1.name)
-print(p1.value)
-print(p1.cost)
-print(p2.name)
-print(p2.value)
-print(p2.cost)
-print(p2.total_duchy)
-
-
-
-total_duchy = 0
-
-counter = tkinter.IntVar()
-def onClick():
-    counter.set(counter.get() - 1)
-def onClick2():
-    counter.set(counter.get() + 1)
-def onClick3():
-    counter.set(counter.get() + 3)
-    global total_duchy
-    total_duchy =  total_duchy + 1
-def onClickduke():
-    counter.set(counter.get() + total_duchy)
-def onClick4():
-    counter.set(counter.get() + 6)
-def onClick5():
-    counter.set(counter.get() + 10)
-
-counter2 = tkinter.IntVar()
-def onClick6():
-    counter2.set(counter2.get() - 1)
-def onClick7():
-    counter2.set(counter2.get() + 1)
-def onClick8():
-    counter2.set(counter2.get() + 3)
-def onClick9():
-    counter2.set(counter2.get() + 6)
-def onClick10():
-    counter2.set(counter2.get() + 10)
+# Functions
+# P1
+def onClick(victory):
+    counter.set(counter.get() + victory.value)
+# P2
+def onClickP2(victory):
+    counter2.set(counter2.get() + victory.value)
 
 counter_1 = Label(root, textvariable = counter, font = 'size, 80')
-counter_2 = Label(root, textvariable = counter2, font = 'size, 80', bg = "#FCF3CF")
-root.wm_attributes("-transparent", "#FCF3CF")
+counter_2 = Label(root, textvariable = counter2, font = 'size, 80')
+#root.wm_attributes("-transparent", "#FCF3CF")
 counter_1.place(x=435, y = 200)
 counter_2.place(x=1485, y = 200)
 
-Button_curse = Button(root, command=onClick, image = curse)
-Button_estate = Button(root, command=onClick2, image = estate)
-Button_duchy = Button(root, command=onClick3, image = duchy)
-Button_province = Button(root, command=onClick4, image = province)
-Button_colony = Button(root, command=onClick5, image = colony)
-Button_duke = Button(root, command=onClickduke, image = duke)
+# Buttons
+# P1
+Button_curse = Button(root, command = lambda: onClick(curse_card), image = curse)
+Button_estate = Button(root, command = lambda: onClick(estate_card), image = estate)
+Button_duchy = Button(root, command = lambda: onClick(duchy_card), image = duchy)
+Button_province = Button(root, command = lambda: onClick(province_card), image = province)
+Button_colony = Button(root, command = lambda: onClick(colony_card), image = colony)
+Button_duke = Button(root, command = lambda: onClick(), image = duke)
 Button_curse.place(x=50, y=400)
 Button_estate.place(x=250, y=400)
 Button_duchy.place(x=450, y=400)
 Button_duke.place(x=50, y=650)
 Button_province.place(x=650, y=400)
 Button_colony.place(x=250, y=650)
-
-Button_curse2 = Button(root, text="Curses", command=onClick6, image = curse)
-Button_estate2 = Button(root, text="Estate", command=onClick7, image = estate)
-Button_duchy2 = Button(root, text="Duchy", command=onClick8, image = duchy)
-Button_province2 = Button(root, text="Province", command=onClick9, image = province)
-Button_colony2 = Button(root, text="Colony", command=onClick10, image = colony)
+# P2
+Button_curse2 = Button(root, text="Curses", command = lambda: onClickP2(curse_card), image = curse)
+Button_estate2 = Button(root, text="Estate", command = lambda: onClickP2(estate_card), image = estate)
+Button_duchy2 = Button(root, text="Duchy", command = lambda: onClickP2(duchy_card), image = duchy)
+Button_province2 = Button(root, text="Province", command = lambda: onClickP2(province_card), image = province)
+Button_colony2 = Button(root, text="Colony", command = lambda: onClickP2(colony_card), image = colony)
 Button_curse2.place(x=1780, y=0)
 Button_estate2.place(x=1780, y=200)
 Button_duchy2.place(x=1780, y=400)
@@ -122,7 +80,5 @@ Button_colony2.place(x=1780, y=800)
 
 can = Canvas(root, bg = 'black', height = 1080, width = 1)
 can.place(relx = 0.5, rely = 0.5, anchor = CENTER)
-
-onClickduke()
 
 root.mainloop()
