62977a72aebc0a906d74eb3db44944971f64c15b
Derek <derekyun820@gmail.com>
2021-06-24 00:30:01 -0500

    Commit 2

--- main.py
+++ main.py
@@ -16,24 +16,24 @@ totalduchy1 = 0
 counter = tkinter.IntVar()
 counter2 = tkinter.IntVar()
 
-# Images
-estate_temp = PhotoImage(file = r"Images\estate.png")
+estate_temp = PhotoImage(file = r"C:\Users\Derek\Desktop\DominionVictory\estate.png")
 estate = estate_temp.subsample(3)
-curse_temp = PhotoImage(file = r"Images\curse.png")
+curse_temp = PhotoImage(file = r"C:\Users\Derek\Desktop\DominionVictory\curse.png")
 curse = curse_temp.subsample(3)
-duchy_temp = PhotoImage(file = r"Images\duchy.png")
+duchy_temp = PhotoImage(file = r"C:\Users\Derek\Desktop\DominionVictory\duchy.png")
 duchy = duchy_temp.subsample(3)
-duke_temp = PhotoImage(file = r"Images\duke.png")
+duke_temp = PhotoImage(file = r"C:\Users\Derek\Desktop\DominionVictory\duke.png")
 duke = duke_temp.subsample(3)
-province_temp = PhotoImage(file = r"Images\province.png")
+province_temp = PhotoImage(file = r"C:\Users\Derek\Desktop\DominionVictory\province.png")
 province = province_temp.subsample(3)
-province_temp = PhotoImage(file = r"Images\colony.png")
+province_temp = PhotoImage(file = r"C:\Users\Derek\Desktop\DominionVictory\colony.png")
 colony = province_temp.subsample(3)
 
 # Cards
 curse_card = Victory("Curse", -1, 0, 0)
 estate_card = Victory("Estate", 1, 2, 0)
 duchy_card = Victory("Duchy", 3, 5, totalduchy1)
+duke_card = Victory("Duke", totalduchy1, 5, 0)
 province_card = Victory("Province", 6, 8, 0)
 colony_card = Victory("Colony", 10, 11, 0)
 
@@ -45,6 +45,10 @@ def onClick(victory):
 # P2
 def onClickP2(victory):
     counter2.set(counter2.get() + victory.value)
+
+def total_cards(victory, counter=None):
+    if victory.name == 'Duchy':
+        counter += 1
 
 counter_1 = Label(root, textvariable = counter, font = 'size, 80')
 counter_2 = Label(root, textvariable = counter2, font = 'size, 80')
@@ -59,24 +63,26 @@ Button_estate = Button(root, command = lambda: onClick(estate_card), image = est
 Button_duchy = Button(root, command = lambda: onClick(duchy_card), image = duchy)
 Button_province = Button(root, command = lambda: onClick(province_card), image = province)
 Button_colony = Button(root, command = lambda: onClick(colony_card), image = colony)
-Button_duke = Button(root, command = lambda: onClick(), image = duke)
+Button_duke = Button(root, command = lambda: onClick(duke_card), image = duke)
 Button_curse.place(x=50, y=400)
 Button_estate.place(x=250, y=400)
 Button_duchy.place(x=450, y=400)
-Button_duke.place(x=50, y=650)
-Button_province.place(x=650, y=400)
+Button_duke.place(x=650, y=400)
+Button_province.place(x=50, y=650)
 Button_colony.place(x=250, y=650)
 # P2
-Button_curse2 = Button(root, text="Curses", command = lambda: onClickP2(curse_card), image = curse)
-Button_estate2 = Button(root, text="Estate", command = lambda: onClickP2(estate_card), image = estate)
-Button_duchy2 = Button(root, text="Duchy", command = lambda: onClickP2(duchy_card), image = duchy)
-Button_province2 = Button(root, text="Province", command = lambda: onClickP2(province_card), image = province)
-Button_colony2 = Button(root, text="Colony", command = lambda: onClickP2(colony_card), image = colony)
-Button_curse2.place(x=1780, y=0)
-Button_estate2.place(x=1780, y=200)
-Button_duchy2.place(x=1780, y=400)
-Button_province2.place(x=1780, y=600)
-Button_colony2.place(x=1780, y=800)
+Button_curse2 = Button(root, command = lambda: onClickP2(curse_card), image = curse)
+Button_estate2 = Button(root, command = lambda: onClickP2(estate_card), image = estate)
+Button_duchy2 = Button(root, command = lambda: onClickP2(duchy_card), image = duchy)
+Button_duke2 = Button(root, command = lambda: onClickP2(duke_card), image = duke)
+Button_province2 = Button(root, command = lambda: onClickP2(province_card), image = province)
+Button_colony2 = Button(root, command = lambda: onClickP2(colony_card), image = colony)
+Button_curse2.place(x=1050, y=400)
+Button_estate2.place(x=1250, y=400)
+Button_duchy2.place(x=1450, y=400)
+Button_duke2.place(x=1650, y=400)
+Button_province2.place(x=1050, y=650)
+Button_colony2.place(x=1250, y=650)
 
 can = Canvas(root, bg = 'black', height = 1080, width = 1)
 can.place(relx = 0.5, rely = 0.5, anchor = CENTER)
