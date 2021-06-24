2aabd20c02d0fc423d0f771ac6bf2b25287c25c9
Derek <derekyun820@gmail.com>
2021-06-24 06:16:14 -0500

    Commit 3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 import tkinter
-from tkinter import Tk, Canvas, Frame, BOTH
+from tkinter import Tk, Canvas
 from Victory import Victory
 
 root = Tk()
@@ -12,14 +12,24 @@ coun = Label(root, image = es)
 coun.place(x=0, y = 0)
 
 # Totals
+global totalduchy1
 totalduchy1 = 0
 counter = tkinter.IntVar()
 counter2 = tkinter.IntVar()
 
-estate_temp = PhotoImage(file = r"Images\estate.png")
-estate = estate_temp.subsample(3)
+
 curse_temp = PhotoImage(file = r"Images\curse.png")
 curse = curse_temp.subsample(3)
+estate_temp = PhotoImage(file = r"Images\estate.png")
+estate = estate_temp.subsample(3)
+mill_temp = PhotoImage(file = r"Images\mill.png")
+mill = mill_temp.subsample(3)
+harem_temp = PhotoImage(file = r"Images\harem.png")
+harem = harem_temp.subsample(3)
+island_temp = PhotoImage(file = r"Images\island.png")
+island = island_temp.subsample(3)
+nobles_temp = PhotoImage(file = r"Images\nobles.png")
+nobles = nobles_temp.subsample(3)
 duchy_temp = PhotoImage(file = r"Images\duchy.png")
 duchy = duchy_temp.subsample(3)
 duke_temp = PhotoImage(file = r"Images\duke.png")
@@ -32,6 +42,10 @@ colony = province_temp.subsample(3)
 # Cards
 curse_card = Victory("Curse", -1, 0, 0)
 estate_card = Victory("Estate", 1, 2, 0)
+mill_card = Victory("Mill", 1, 4, 0)
+harem_card = Victory("Harem", 1, 6, 0)
+island_card = Victory("Island", 1, 4, 0)
+nobles_card = Victory("Nobles", 1, 6, 0)
 duchy_card = Victory("Duchy", 3, 5, totalduchy1)
 duke_card = Victory("Duke", totalduchy1, 5, 0)
 province_card = Victory("Province", 6, 8, 0)
@@ -46,9 +60,9 @@ def onClick(victory):
 def onClickP2(victory):
     counter2.set(counter2.get() + victory.value)
 
-def total_cards(victory, counter=None):
+def total_cards(victory, totalduchy1=None):
     if victory.name == 'Duchy':
-        counter += 1
+        totalduchy1 += 1
 
 counter_1 = Label(root, textvariable = counter, font = 'size, 80')
 counter_2 = Label(root, textvariable = counter2, font = 'size, 80')
@@ -60,10 +74,14 @@ counter_2.place(x=1485, y = 200)
 # P1
 Button_curse = Button(root, command = lambda: onClick(curse_card), image = curse)
 Button_estate = Button(root, command = lambda: onClick(estate_card), image = estate)
+Button_mill = Button(root, command = lambda: onClick(mill_card), image = mill)
+Button_harem = Button(root, command = lambda: onClick(harem_card), image = harem)
+Button_island = Button(root, command = lambda: onClick(island_card), image = island)
+Button_nobles = Button(root, command = lambda: onClick(nobles_card), image = nobles)
 Button_duchy = Button(root, command = lambda: onClick(duchy_card), image = duchy)
+Button_duke = Button(root, command = lambda: onClick(duke_card), image = duke)
 Button_province = Button(root, command = lambda: onClick(province_card), image = province)
 Button_colony = Button(root, command = lambda: onClick(colony_card), image = colony)
-Button_duke = Button(root, command = lambda: onClick(duke_card), image = duke)
 Button_curse.place(x=50, y=400)
 Button_estate.place(x=250, y=400)
 Button_duchy.place(x=450, y=400)
@@ -73,6 +91,10 @@ Button_colony.place(x=250, y=650)
 # P2
 Button_curse2 = Button(root, command = lambda: onClickP2(curse_card), image = curse)
 Button_estate2 = Button(root, command = lambda: onClickP2(estate_card), image = estate)
+Button_mill2 = Button(root, command = lambda: onClick(mill_card), image = mill)
+Button_harem2 = Button(root, command = lambda: onClick(harem_card), image = harem)
+Button_island2 = Button(root, command = lambda: onClick(island_card), image = island)
+Button_nobles2 = Button(root, command = lambda: onClick(nobles_card), image = nobles)
 Button_duchy2 = Button(root, command = lambda: onClickP2(duchy_card), image = duchy)
 Button_duke2 = Button(root, command = lambda: onClickP2(duke_card), image = duke)
 Button_province2 = Button(root, command = lambda: onClickP2(province_card), image = province)
