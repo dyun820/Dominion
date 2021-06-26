61bbc01c4010169429dbaae334c060bd09301fee
Derek <derekyun820@gmail.com>
2021-06-26 03:54:52 -0500

    Commit 4.9

--- main.py
+++ main.py
@@ -14,15 +14,20 @@ coun.place(x=0, y = 0)
 
 # Totals
 total_silver = 0
+total_silver2 = 0
+feodum_count = 0
+feodum_count2 = 0
 totalduchy1 = 0
 totalduchy2 = 0
 total_victory = 0
 total_victory2 = 0
+silk_road_count = 0
+silk_road_count2 = 0
 counter = tkinter.IntVar()
 counter2 = tkinter.IntVar()
 
 silver_temp = PhotoImage(file = r"Images\silver.png")
-silver = silver_temp.subsample(3)
+silver = silver_temp.subsample(5)
 curse_temp = PhotoImage(file = r"Images\curse.png")
 curse = curse_temp.subsample(3)
 estate_temp = PhotoImage(file = r"Images\estate.png")
@@ -35,26 +40,22 @@ feodum_temp = PhotoImage(file = r"Images\feodum.png")
 feodum = feodum_temp.subsample(3)
 silk_road_temp = PhotoImage(file = r"Images\silk_road.png")
 silk_road = silk_road_temp.subsample(3)
-
 castle_temp = PhotoImage(file = r"Images\castle1.png")
 castle = castle_temp.subsample(3)
-fairgrounds_temp = PhotoImage(file = r"Images\fairgrounds.png.png")
+fairgrounds_temp = PhotoImage(file = r"Images\fairgrounds.png")
 fairgrounds = fairgrounds_temp.subsample(3)
 distant_lands_temp = PhotoImage(file = r"Images\distant_lands.png")
 distant_lands = distant_lands_temp.subsample(3)
 gardens_temp = PhotoImage(file = r"Images\gardens.png")
 gardens = gardens_temp.subsample(3)
-
 cemetery_temp = PhotoImage(file = r"Images\cemetery.png")
 cemetery = cemetery_temp.subsample(3)
-
 tunnel_temp = PhotoImage(file = r"Images\tunnel.png")
 tunnel = tunnel_temp.subsample(3)
 farmland_temp = PhotoImage(file = r"Images\farmland.png")
 farmland = farmland_temp.subsample(3)
 dame_josephine_temp = PhotoImage(file = r"Images\dame_josephine.png")
 dame_josephine = dame_josephine_temp.subsample(3)
-
 harem_temp = PhotoImage(file = r"Images\harem.png")
 harem = harem_temp.subsample(3)
 island_temp = PhotoImage(file = r"Images\island.png")
@@ -71,25 +72,22 @@ province_temp = PhotoImage(file = r"Images\colony.png")
 colony = province_temp.subsample(3)
 
 # Cards
-silver_card = Victory("Silver", -5, 0)
+silver_card = Victory("Silver", -1, 0)
 curse_card = Victory("Curse", -1, 0)
 estate_card = Victory("Estate", 1, 2)
 great_hall_card = Victory("Great Hall", 1, 3)
 mill_card = Victory("Mill", 1, 4)
 feodum_card = Victory("Feodum", total_silver, 4)
 silk_road_card = Victory("Silk Road", total_victory, 4)
-
 castle1_card = Victory("Castle 1", 1, 3)
+castle1_2_card = Victory("Castle 1", 1, 3)
 fairgrounds_card = Victory("Fairgrounds", 2, 5)
 distant_lands_card = Victory("Distant Lands", 4, 5)
 gardens_card = Victory("Gardens", 1, 4)
-
 cemetery_card = Victory("Cemetery", 2, 4)
-
 tunnel_card = Victory("Tunnel", 2, 5)
 farmland_card = Victory("Farmland", 2, 6)
 dame_josephine_card = Victory("Dame Josephine", 2, 5)
-
 harem_card = Victory("Harem", 2, 6)
 island_card = Victory("Island", 2, 4)
 nobles_card = Victory("Nobles", 2, 6)
@@ -104,9 +102,18 @@ colony_card = Victory("Colony", 10, 11)
 def onClick(victory):
     global totalduchy1
     global total_victory
+    global feodum_count
+    global total_silver
+    global silk_road_count
     counter.set(counter.get() + victory.value)
+    if victory.name == 'Silver':
+        total_silver += 1
+        feodum_count = math.floor(total_silver / 3)
+    if victory.name == 'Feodum':
+        counter.set(counter.get() + feodum_count)
     if victory.value >= 0:
-        total_victory += 1
+        silk_road_count += 1
+        total_victory = math.floor(silk_road_count / 4)
     if victory.name == 'Silk Road':
         counter.set(counter.get() + total_victory)
     if victory.name == 'Duchy':
@@ -117,9 +124,18 @@ def onClick(victory):
 def onClickP2(victory):
     global totalduchy2
     global total_victory2
+    global feodum_count2
+    global total_silver2
+    global silk_road_count2
     counter2.set(counter2.get() + victory.value)
+    if victory.name == 'Silver':
+        total_silver2 += 1
+        feodum_count2 = math.floor(total_silver2 / 3)
+    if victory.name == 'Feodum':
+        counter2.set(counter2.get() + feodum_count2)
     if victory.value >= 0:
-        total_victory2 += 1
+        silk_road_count2 += 1
+        total_victory2 = math.floor(silk_road_count2 / 4)
     if victory.name == 'Silk Road':
         counter2.set(counter2.get() + total_victory2)
     if victory.name == 'Duchy':
@@ -127,12 +143,12 @@ def onClickP2(victory):
     if victory.name == 'Duke':
         counter2.set(counter2.get() + totalduchy1)
 
-
+# Totals
 counter_1 = Label(root, textvariable = counter, font = 'size, 90')
 counter_2 = Label(root, textvariable = counter2, font = 'size, 90')
 #root.wm_attributes("-transparent", "#FCF3CF")
-counter_1.place(x=650, y = 0)
-counter_2.place(x=1485, y = 0)
+counter_1.place(x=650, y = 10)
+counter_2.place(x=1180, y = 0)
 
 # Buttons
 # P1
@@ -158,27 +174,27 @@ Button_duchy = Button(root, command = lambda: onClick(duchy_card), image = duchy
 Button_duke = Button(root, command = lambda: onClick(duke_card), image = duke)
 Button_province = Button(root, command = lambda: onClick(province_card), image = province)
 Button_colony = Button(root, command = lambda: onClick(colony_card), image = colony)
-# Button_silver.place(x=20, y=150)
+Button_silver.place(x=347, y=10)
 Button_curse.place(x=20, y=150)
 Button_estate.place(x=170, y=150)
 Button_great_hall.place(x=320, y=150)
 Button_mill.place(x=470, y=150)
 Button_feodum.place(x=620, y=150)
 Button_silk_road.place(x=770, y=150)
-Button_castle1.place(x=20, y=150)
-Button_fairgrounds.place(x=170, y=150)
-Button_distant_lands.place(x=320, y=150)
-Button_gardens.place(x=470, y=150)
-Button_cemetery.place(x=620, y=150)
-Button_tunnel.place(x=770, y=150)
-Button_farmland.place(x=20, y=150)
-Button_dame_josephine.place(x=170, y=150)
-Button_harem.place(x=320, y=370)
-Button_island.place(x=470, y=370)
-Button_nobles.place(x=620, y=370)
-Button_duchy.place(x=770, y=370)
-Button_duke.place(x=20, y=370)
-Button_province.place(x=170, y=590)
+Button_castle1.place(x=20, y=370)
+Button_fairgrounds.place(x=170, y=370)
+Button_distant_lands.place(x=320, y=370)
+Button_gardens.place(x=470, y=370)
+Button_cemetery.place(x=620, y=370)
+Button_tunnel.place(x=770, y=370)
+Button_farmland.place(x=20, y=590)
+Button_dame_josephine.place(x=170, y=590)
+Button_harem.place(x=320, y=590)
+Button_island.place(x=470, y=590)
+Button_nobles.place(x=620, y=590)
+Button_duchy.place(x=770, y=590)
+Button_duke.place(x=20, y=810)
+Button_province.place(x=170, y=810)
 Button_colony.place(x=320, y=810)
 # P2
 Button_silver2 = Button(root, command = lambda: onClickP2(silver_card), image = silver)
@@ -188,7 +204,7 @@ Button_great_hall2 = Button(root, command = lambda: onClickP2(great_hall_card), 
 Button_mill2 = Button(root, command = lambda: onClickP2(mill_card), image = mill)
 Button_feodum2 = Button(root, command = lambda: onClickP2(feodum_card), image = feodum)
 Button_silk_road2 = Button(root, command = lambda: onClickP2(silk_road_card), image = silk_road)
-# Button_castle1 = Button(root, command = lambda: onClick(castle1_card), image = castle)
+Button_castle1_2 = Button(root, command = lambda: onClickP2(castle1_2_card), image = castle)
 Button_fairgrounds2 = Button(root, command = lambda: onClickP2(fairgrounds_card), image = fairgrounds)
 Button_distant_lands2 = Button(root, command = lambda: onClickP2(distant_lands_card), image = distant_lands)
 Button_gardens2 = Button(root, command = lambda: onClickP2(gardens_card), image = gardens)
@@ -203,12 +219,28 @@ Button_duchy2 = Button(root, command = lambda: onClickP2(duchy_card), image = du
 Button_duke2 = Button(root, command = lambda: onClickP2(duke_card), image = duke)
 Button_province2 = Button(root, command = lambda: onClickP2(province_card), image = province)
 Button_colony2 = Button(root, command = lambda: onClickP2(colony_card), image = colony)
-Button_curse2.place(x=1050, y=400)
-Button_estate2.place(x=1250, y=400)
-Button_duchy2.place(x=1450, y=400)
-Button_duke2.place(x=1650, y=400)
-Button_province2.place(x=1050, y=650)
-Button_colony2.place(x=1250, y=650)
+Button_silver2.place(x=1473, y=10)
+Button_curse2.place(x=1000, y=150)
+Button_estate2.place(x=1150, y=150)
+Button_great_hall2.place(x=1300, y=150)
+Button_mill2.place(x=1450, y=150)
+Button_feodum2.place(x=1600, y=150)
+Button_silk_road2.place(x=1750, y=150)
+Button_castle1_2.place(x=1000, y=370)
+Button_fairgrounds2.place(x=1159, y=370)
+Button_distant_lands2.place(x=1300, y=370)
+Button_gardens2.place(x=1450, y=370)
+Button_cemetery2.place(x=1600, y=370)
+Button_tunnel2.place(x=1750, y=370)
+Button_farmland2.place(x=1000, y=590)
+Button_dame_josephine2.place(x=1150, y=590)
+Button_harem2.place(x=1300, y=590)
+Button_island2.place(x=1450, y=590)
+Button_nobles2.place(x=1600, y=590)
+Button_duchy2.place(x=1750, y=590)
+Button_duke2.place(x=1000, y=810)
+Button_province2.place(x=1150, y=810)
+Button_colony2.place(x=1300, y=810)
 
 can = Canvas(root, bg = 'black', height = 1080, width = 1)
 can.place(relx = 0.5, rely = 0.5, anchor = CENTER)
